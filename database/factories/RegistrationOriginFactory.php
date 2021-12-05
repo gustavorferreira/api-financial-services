217c22070d3e8345f4fb5e0a80c3f943628c591d
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2021-12-05 11:59:58 -0300

    [DEL] - unusable statement

--- database/factories/RegistrationOriginFactory.php
+++ database/factories/RegistrationOriginFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\RegistrationOrigin;
 
 class RegistrationOriginFactory extends Factory
 {
--- database/factories/StatusFactory.php
+++ database/factories/StatusFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Status;
 
 class StatusFactory extends Factory
 {
