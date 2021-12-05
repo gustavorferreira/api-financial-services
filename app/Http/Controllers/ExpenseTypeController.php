0ed254b71d315b0d896622abbffe819fccd1fe73
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2021-12-05 15:57:47 -0300

    [FIX] - Fix name from plural to singular of Classes

--- app/Http/Controllers/ExpenseTypeController.php
+++ app/Http/Controllers/ExpenseTypeController.php
@@ -6,7 +6,7 @@ use App\Http\Requests\StoreExpenseTypeRequest;
 use App\Http\Requests\UpdateExpenseTypeRequest;
 use App\Models\ExpenseType;
 
-class ExpensesTypesController extends Controller
+class ExpenseTypeController extends Controller
 {
     /**
      * Display a listing of the resource.
